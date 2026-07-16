892e571fddc973bc3ce2b8c8e382ad5e79169f9c
tishroma99-wq <tishroma99@gmail.com>
2026-07-16 09:24:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://tishroma99-wq.github.io/web-4")
+ADMIN_IDS = [int(x) for x in os.getenv("8547928521", "").split(",") if x]
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://tishroma99-wq.github.io/my-web1/")
 DATABASE_PATH = os.getenv("DATABASE_PATH", "trustgram.db")
-API_URL = os.getenv("API_URL", "http://localhost:8000")
+API_URL = os.getenv("API_URL", "http://trustworthy-rebirth-production-fe8b.up.railway.app")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN не найден. Проверь файл .env")
